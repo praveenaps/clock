85e8a860b8e4f77a1533f45b90e18a129c94515c
praveena <praveena@optisolbusiness.com>
2021-09-28 13:59:13 +0530

    fourth commit

--- src/test/java/alarm/alarm1/AppTest.java
+++ src/test/java/alarm/alarm1/AppTest.java
@@ -1,10 +1,8 @@
 package alarm.alarm1;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import alarm.alarm1.App;
 

e3be7e1ead0359c8cf44dbeda6121f3d1ee0cca4
praveena <praveena@optisolbusiness.com>
2021-09-28 13:02:58 +0530

    third commit

--- src/main/java/alarm/alarm1/App.java
+++ src/main/java/alarm/alarm1/App.java
@@ -1,13 +1,83 @@
 package alarm.alarm1;
 
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.windows.WindowsDriver;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+   /* public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
+    }*/
+public static WindowsDriver AlarmClockSession=null;
+	
+	public static void initialization() throws Exception
+	{
+	
+        DesiredCapabilities appCapabilities = new DesiredCapabilities();
+        appCapabilities.setCapability("app", "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App");
+        AlarmClockSession = new WindowsDriver(new URL("http://127.0.0.1:4723"), appCapabilities);
+       
+     }
+	
+	public static void Addalarm() throws InterruptedException
+	{
+		Thread.sleep(2000);
+		AlarmClockSession.findElementByName("Alarm").click();
+		
+		Thread.sleep(4000);
+		AlarmClockSession.findElementByName("Add an alarm").click();
+		
+		AlarmClockSession.findElementByAccessibilityId("HourPicker").sendKeys("04");
+	
+		AlarmClockSession.findElementByAccessibilityId("MinutePicker").sendKeys("3");
+	
+		AlarmClockSession.findElementByName("Alarm name").clear();
+		AlarmClockSession.findElementByName("Alarm name").sendKeys("test");
+
+		AlarmClockSession.findElementByAccessibilityId("RepeatCheckBox").click();
+
+		AlarmClockSession.findElementByName("Tuesday").click();
+	
+		AlarmClockSession.findElementByName("Save").click();
+		Thread.sleep(4000);
+		AlarmClockSession.findElementByAccessibilityId("EditAlarmsButton").click();
+		System.out.println("test");
+		
+
+			AlarmClockSession.findElementByAccessibilityId("DeleteButton").click();
+
+	
+		
+	}
+	public static void Addalarm1() throws InterruptedException
+	{
+		Thread.sleep(2000);
+		AlarmClockSession.findElementByName("Alarm").click();
+		
+		Thread.sleep(4000);
+		AlarmClockSession.findElementByName("Add an alarm").click();
+		
+		AlarmClockSession.findElementByAccessibilityId("HourPicker").sendKeys("04");
+	
+		AlarmClockSession.findElementByAccessibilityId("MinutePicker").sendKeys("3");
+	
+		AlarmClockSession.findElementByName("Alarm name").clear();
+		AlarmClockSession.findElementByName("Alarm name").sendKeys("test");
+
+		AlarmClockSession.findElementByAccessibilityId("RepeatCheckBox").click();
+
+		AlarmClockSession.findElementByName("Tuesday").click();
+	
+		AlarmClockSession.findElementByName("Save").click();
+
+	}
 }
+
--- src/test/java/alarm/alarm1/AppTest.java
+++ src/test/java/alarm/alarm1/AppTest.java
@@ -3,18 +3,39 @@ package alarm.alarm1;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+import alarm.alarm1.App;
+
+public class AppTest extends App{
+	App almclk;
+	   
+	   @BeforeMethod
+	   public void setup() throws Exception
+	   {
+		   App.initialization();
+	   
+	   }
+		  @Test
+			public void add() throws Exception
+			{ 
+			  App.Addalarm();
+			  
+			}
+		  
+		  @Test
+			public void add1() throws Exception
+			{ 
+			  App.Addalarm1();
+			  
+			}
+		
+			@AfterMethod
+			public void teardown()
+			{
+				AlarmClockSession.quit();
+				
+			} 
+
 }

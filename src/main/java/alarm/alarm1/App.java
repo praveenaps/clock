2f68858d9a1a30a37365e0bb4c0e04f537fb39f9
praveena <praveena@optisolbusiness.com>
2021-09-28 12:21:59 +0530

    first commit

---
+++ src/main/java/alarm/alarm1/App.java
@@ -0,0 +1,13 @@
+package alarm.alarm1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
